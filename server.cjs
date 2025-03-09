42e2f711622bfafe9e29511b9d3bd40da166cefc
SIUUU42 <trishaykaul@gmail.com>
2025-03-09 12:53:42 +0530

    Update server.cjs

--- server.cjs
+++ server.cjs
@@ -10,11 +10,11 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public'))); // Serve static frontend files
+app.use(express.static(path.join(__dirname, 'MIC'))); // Serve static frontend files
 
 // Linking the backend to the front end
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Example of the API Endpoint
